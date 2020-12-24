c8c0d88f8cdbe6594dbb62de69cc86de2cecafbc
Emanuel Saramago <esaramago@slbenfica.pt>
2020-12-24 15:19:53 +0000

    dist folder

---
+++ dist/precache-manifest.72b2533e32c596398c6846ce7511dfb2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8465aeaeafe7154d829f",
+    "url": "/css/app.ee0566ed.css"
+  },
+  {
+    "revision": "2866ef8031a2e3171534",
+    "url": "/css/chunk-7e7d4298.0761c4fb.css"
+  },
+  {
+    "revision": "51eac048ef6698029186",
+    "url": "/css/chunk-d1d45dec.3dfd833f.css"
+  },
+  {
+    "revision": "eafd1886a108b0ee5b58e57e5ca540fb",
+    "url": "/img/sprite.svg"
+  },
+  {
+    "revision": "5707c908f70741caf97fe1d2229ebea0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8465aeaeafe7154d829f",
+    "url": "/js/app.582482f2.js"
+  },
+  {
+    "revision": "43fc42875c70ec916b4f",
+    "url": "/js/chunk-2d0b257b.76374a30.js"
+  },
+  {
+    "revision": "2866ef8031a2e3171534",
+    "url": "/js/chunk-7e7d4298.b8830df2.js"
+  },
+  {
+    "revision": "51eac048ef6698029186",
+    "url": "/js/chunk-d1d45dec.abf0b8c6.js"
+  },
+  {
+    "revision": "663396fdb4123a4244cf",
+    "url": "/js/chunk-vendors.a343adfe.js"
+  },
+  {
+    "revision": "06852693347a43ca329030217b7bed01",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
